67f71c584de724eb175b0bf5f9774b706da00923
atrox3d <atrox3d@gmail.com>
2024-12-22 23:04:50 +0100

    add and rename scripts

--- scripts/dump_procedures.sh
+++ scripts/dump_procedures.sh
@@ -19,11 +19,19 @@ else
     SQL_FILE="procedures.sql"
 fi
 
-echo "DATABASE....: ${DATABASE}"
-echo "SQL_FILE....: ${SQL_FILE}"
+DOCKER_OUTPUT_DIR=out
+DOCKER_INPUT_DIR=in
 
+echo "DOCKER_INPUT_DIR.....: ${DOCKER_INPUT_DIR}"
+echo "DOCKER_OUTPUT_DIR....: ${DOCKER_OUTPUT_DIR}"
+echo "DATABASE.............: ${DATABASE}"
+echo "SQL_FILE.............: ${SQL_FILE}"
 
 
 echo docker compose exec -it \
     db /bin/bash -c \
-    "mysqldump -u root -p --routines --no-create-info --no-data --no-create-db --skip-opt --skip-triggers ${DATABASE} > /out/${SQL_FILE}"
+    "mysqldump -u root -p --routines --no-create-info --no-data --no-create-db --skip-opt --skip-triggers ${DATABASE} > /${DOCKER_OUTPUT_DIR}/${SQL_FILE}"
+
+docker compose exec -it \
+    db /bin/bash -c \
+    "mysqldump -u root -p --routines --no-create-info --no-data --no-create-db --skip-opt --skip-triggers ${DATABASE} > /${DOCKER_OUTPUT_DIR}/${SQL_FILE}"
